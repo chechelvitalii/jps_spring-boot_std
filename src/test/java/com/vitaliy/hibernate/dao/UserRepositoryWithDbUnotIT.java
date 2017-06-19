4d7e92e15faed2e145433b9158fd2e27375d5fbf
chechelvitalii <chechelvitaliy@gmail.com>
2017-06-20 00:43:42 +0300

    fixed dbUnit

--- src/test/java/com/vitaliy/hibernate/dao/UserRepositoryWithDbUnotIT.java
+++ src/test/java/com/vitaliy/hibernate/dao/UserRepositoryWithDbUnotIT.java
@@ -4,7 +4,6 @@ import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
-import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.vitaliy.hibernate.HibernateApplication;
 import com.vitaliy.hibernate.model.User;
 import org.junit.Test;
@@ -14,7 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import java.util.List;
@@ -24,25 +22,28 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = HibernateApplication.class) //TODO resolve how to set up test config here ?
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
+@TestExecutionListeners({
         TransactionalTestExecutionListener.class,
-        DbUnitTestExecutionListener.class})
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
+
 public class UserRepositoryWithDbUnotIT {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    @DatabaseSetup("userData.xml")
-    @DatabaseTearDown(type = DatabaseOperation.DELETE_ALL)
     //TODO in future @ExpectedDatabase("expectedData.xml")
+    @Test
+    /**type = DatabaseOperation.CLEAN_INSERT default for DatabaseSetup*/
+    @DatabaseSetup("/userData.xml")
+    @DatabaseTearDown(type = DatabaseOperation.DELETE_ALL)
     public void saveUser() throws Exception {
         //GIVEN
         //WHEN
         List<User> allUsers = userRepository.finAll();
         //THEN
-        assertThat(allUsers,hasSize(3));
+        assertThat(allUsers, hasSize(3));
     }
 
 }
